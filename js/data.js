0a0cc321e21511be25f6e7f7ab71f44907e2fe38
sunbangbang <1304695949@qq.com>
2022-12-23 09:19:23 +0800

    提交代码

---
+++ js/data.js
@@ -0,0 +1,33 @@
+var dataJson = [
+    {
+        "hash": "1111111111111111",
+        "url": "./music/你是人间四月天.mp3",
+        "name": "解忧邵帅",
+        "song_name": "你是人间四月天",
+        "audio_name": "解忧邵帅 - 你是人间四月天",
+        "lyrics": "[00:00.000] 作曲 : 解忧邵帅[00:01.000] 作词 : 解忧邵帅[00:24.14]跨越山河去拥抱你，多大风浪都在一起[00:32.31]听到最美的记忆，关于你所有的消息。[00:40.37]盼望每天都看到你，雨天也风和日丽。[00:48.28]花花绿绿的世界里，我只会喜欢你。[00:56.15]愿你在我看不到的地方安然无恙，[01:00.09]愿你的冬天永远不缺暖阳。[01:03.99]愿你的明天不再经历雨打风霜，[01:08.06]愿你的未来永远热泪盈眶。[01:28.33]最美好的年纪里，柴米油盐酱醋你。[01:36.59]春风把我带给你，四月润湿了空气。[01:44.40]不温柔的世界里，你是我的运气。[01:52.77]我全部的好脾气，因为我爱你。[02:00.15]愿我在六十岁的时候陪你看看夕阳，[02:04.12]愿我在想起你嘴角微微上扬。[02:08.03]愿我在北方的冬天为你披件衣裳，[02:12.05]愿我在一贫如洗能有你在身旁。[02:48.04]愿你在我看不到的地方安然无恙，[02:52.06]愿你的冬天永远不缺暖阳。[02:56.11]愿你的明天不再经历雨打风霜，[03:00.03]愿你的未来永远热泪盈眶。[03:04.11]愿我在六十岁的时候陪你看看夕阳，[03:08.02]愿我在想起你嘴角微微上扬。[03:12.03]愿我在北方的冬天为你披件衣裳，[03:16.18]愿我在一贫如洗能有你在身旁。[03:20.84]制作人：唐显程[03:22.39]编曲：唐显程 张东升[03:23.59]吉他：张东升[03:24.99]和声：邵帅 唐显程 张东升[03:26.24]录音棚：郑州木纹之声[03:27.49]混音：唐显程[03:28.80]封面：刘扬[03:30.40]祝有情人，白头到老。[03:32.66]",
+        "img": "images/109951164252455813.jpg",
+        "album_name":"你是人间四月天"
+    },
+    {
+        "hash": "028D3B9421BE8425D60DC57735CF6EBC",
+        "url": "./music/起风了.mp3",
+        "name": "买辣椒也用券",
+        "song_name": "起风了",
+        "audio_name": "买辣椒也用券 - 起风了",
+        "lyrics": "[00:00.000] 作曲 : 高橋優[00:00.464] 作词 : 米果[00:01.394] 编曲：池窪浩一 (Kouichi Ikekubo)[00:26.137] 这一路上走走停停[00:29.192] 顺着少年漂流的痕迹[00:32.281] 迈出车站的前一刻[00:35.395] 竟有些犹豫[00:38.394] 不禁笑这近乡情怯[00:41.791] 仍无可避免[00:44.419] 而长野的天[00:45.734] 依旧那么暖[00:47.284] 风吹起了从前[00:50.053] 从前初识这世间[00:52.785] 万般流连[00:54.562] 看着天边似在眼前[00:57.698] 也甘愿赴汤蹈火去走它一遍[01:02.570] 如今走过这世间[01:05.605] 万般流连[01:07.236] 翻过岁月不同侧脸[01:10.466] 措不及防闯入你的笑颜[01:16.113] 我曾难自拔于世界之大[01:20.207] 也沉溺于其中梦话[01:23.441] 不得真假 不做挣扎 不惧笑话[01:29.101] 我曾将青春翻涌成她[01:32.530] 也曾指尖弹出盛夏[01:35.881] 心之所动 且就随缘去吧[01:42.357] 逆着光行走 任风吹雨打[01:58.630] 短短的路走走停停[02:01.771] 也有了几分的距离[02:04.841] 不知抚摸的是故事 还是段心情[02:11.318] 也许期待的不过是 与时间为敌[02:17.098] 再次看到你[02:18.594] 微凉晨光里[02:20.161] 笑得很甜蜜[02:22.723] 从前初识这世间[02:25.542] 万般流连[02:27.270] 看着天边似在眼前[02:30.220] 也甘愿赴汤蹈火去走它一遍[02:35.313] 如今走过这世间[02:38.262] 万般流连[02:39.991] 翻过岁月不同侧脸[02:42.962] 措不及防闯入你的笑颜[02:48.870] 我曾难自拔于世界之大[02:52.942] 也沉溺于其中梦话[02:56.145] 不得真假 不做挣扎 不惧笑话[03:01.742] 我曾将青春翻涌成她[03:05.839] 也曾指尖弹出盛夏[03:08.669] 心之所动 且就随缘去吧[03:40.529] 晚风吹起你鬓间的白发[03:44.178] 抚平回忆留下的疤[03:47.391] 你的眼中 明暗交杂 一笑生花[03:52.900] 暮色遮住你蹒跚的步伐[03:56.932] 走进床头藏起的画[04:00.146] 画中的你 低着头说话[04:05.680] 我仍感叹于世界之大[04:09.391] 也沉醉于儿时情话[04:12.756] 不剩真假 不做挣扎 无谓笑话[04:18.529] 我终将青春还给了她[04:22.281] 连同指尖弹出的盛夏[04:25.609] 心之所动 就随风去了[04:31.204] 以爱之名 你还愿意吗[04:39.982] 吉他： 池窪浩一 (Kouichi Ikekubo)[04:40.994] 贝斯：小島剛広 (Takehiro Kojima)[04:41.661] 鼓：吉田佳史 (TRICERATOPS) (Yoshifumi Yoshida(TRICERATOPS))[04:42.463] 键盘：平畑徹也 (Tetsuya Hirahata)[04:43.341] 小提琴：須磨和声 (Wasei Suma)[04:44.211] 弦乐：須磨和声、池窪浩一[04:46.105] 录音工程：玉乃井光紀 (Mitsunori Tamanoi) - studioFine[04:46.760] 发行公司：Negia Entertainment Inc.[04:47.476] 特别支持：中村光雄(Mitsuo Nakamura)[04:51.019]",
+        "img": "images/109951163699673355.jpg",
+        "album_name":"起风了"
+    },
+    {
+        "hash": "BCAF29F609DFABAE6BC08FBD8BA7D24D",
+        "url": "./music/Dancing With Your Ghost.mp3",
+        "name": "Sasha Sloan",
+        "song_name": "Dancing With Your Ghost",
+        "audio_name": "Sasha Sloan - Dancing With Your Ghost",
+        "lyrics": "[by:Trap_Girl][00:00.000] 作曲 : Sasha Sloan[00:01.000] 作词 : Sasha Sloan[00:12.47]Yelling at the sky[00:15.37]Screaming at the world[00:18.51]Baby, why'd your go away?[00:21.17]I’m still your girl[00:25.00]Holding on too tight[00:28.16]Head up in the clouds[00:31.51]Heaven only knows where you are now[00:38.08]How do I love, how do I love again?[00:44.35]How do I trust, how do I trust again?[00:50.10]I stay up all night[00:52.73]Tell myself I'm alright[00:55.63]Baby, you're just harder to see than most[01:02.27]I put the record on[01:05.17]Wait ’til I hear our song[01:08.53]Every night I'm dancing with your ghost[01:15.23]Every night I'm dancing with your ghost[01:22.24]Never got the chance[01:25.49]To say a last goodbye[01:29.01]I gotta move on[01:31.62]But it hurts to try[01:34.55]How do I love, how do I love again?[01:41.25]How do I trust, how do I trust again?[01:47.83]I stay up all night[01:50.11]Tell myself I'm alright[01:53.28]Baby, you're just harder to see than most[01:59.35]I put the record on[02:02.93]Wait 'til I hear our song[02:06.13]Every night I'm dancing with your ghost[02:12.77]Every night I'm dancing with your ghost[02:20.14]How do I love, how do I love again?[02:26.32]How do I trust, how do I trust again?[02:32.51]I stay up all night[02:34.84]Tell myself I’m alright[02:38.01]Baby, you’re just harder to see than most[02:45.21]I put the record on[02:47.72]Wait 'til I hear our song[02:50.94]Every night I’m dancing with your ghost[02:57.44]Every night I'm dancing with your ghost[03:03.82]Every night I'm dancing with your ghost",
+        "img": "images/20190626210118627550.jpg",
+        "album_name":"Dancing With Your Ghost"
+    }
+    
+]  
